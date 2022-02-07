7ac205662f13a2c07a1e13475a6a5b0f7ffe33a9
thibault.serabian <thibault.serabian@epita.fr>
2022-02-07 23:50:05 +0100

    documentation

---
+++ README.md
@@ -0,0 +1,151 @@
+# Cluster
+
+Cluster is a crate that define the Cluster and Node traits.
+The trait cluster is a Trait to represent Graph data structure.
+
+## Implementing Cluster.
+
+To implement Cluster for the structure of you choice, you must before implement a structure that implement to Trait Node.
+
+### Functions to implement :
+
+---
+
+#### get
+
+Get a node from the graph.
+
+**Parameter**
+
+- key - the index of the node in the Graph. 
+
+**Returns**
+
+An option containing an immutable reference to the Node if present in the Graph, returns None otherwise.
+
+```rust
+fn get(&self, key: &K) -> Option<&N>;
+```
+
+---
+
+#### get_mut
+
+Get a node from the graph.
+
+**Parameter**
+
+- key - the index of the node in the Graph.
+
+**Returns**
+
+An option containing a mutable reference to the Node if present in the Graph, returns None otherwise.
+
+```rust
+fn get_mut(&mut self, key: &K) -> Option<&mut N>;
+```
+
+---
+
+#### new_key
+
+Generate a key that is not already used in the graph and returns it.
+
+**Return**
+
+The key newly generated
+
+```rust
+fn new_key(&self) -> K;
+```
+
+#### contains_key
+
+Check if the Cluster contains a node at a given key.
+
+**Parameter**
+
+- key - The key on we want to check the Cluster contains it or no.
+
+```rust
+fn contains_key(&self, key: &K) -> bool;
+```
+
+---
+
+#### add
+
+Add a node in the Cluster.
+
+**Return**
+
+The index at which the node has been stored in the graph.
+
+```rust
+fn add(&mut self, node: N) -> K;
+```
+
+---
+
+#### remove
+
+Removes the designated Node from the graph.
+
+**Parameter**
+
+- key - The key of the Node to remove.
+
+```rust
+fn remove(&mut self, key: K) -> Result<()>;
+```
+
+---
+
+
+
+## Implementing Node.
+
+### Functions to implement :
+
+**adj**
+
+Get the adjacency of the current Node.
+
+**Return**
+
+A immutable reference to the adjacency list of the current Node.
+
+```rust
+fn adj(&self) -> &Vec<K>;
+```
+
+---
+
+**adj_mut**
+
+Get the adjacency of the current Node.
+
+**Return**
+
+A immutable reference to the adjacency list of the current Node.
+
+```rust
+fn adj_mut(&mut self) -> &mut Vec<K>;
+```
+
+---
+
+## Help
+
+## Remainder
+
+```mermaid
+graph LR
+0 --> 1;
+1 --> 2;
+2 --> 3;
+0 --> 3;
+3 --> 1
+```
+
+> For this Cluster (graph), the adjacency list of the node 0 would be [1, 3];
--- cluster/src/lib.rs
+++ cluster/src/lib.rs
@@ -21,30 +21,49 @@ impl Display for ClusterError {
 }
 impl Error for ClusterError {}
 impl ClusterError {
+    /// Create a new boxed Cluster error with no details.
+    /// # Return
+    /// The newly created Boxed ClusterError.
     pub fn new_boxed() -> Box<ClusterError> {
         Box::new(ClusterError {
             detail: String::from(""),
         })
     }
 
-    pub fn detailled_boxed(message: &str) -> Box<ClusterError> {
+    /// Create a new boxed Cluster error with the specified details in it.
+    /// # Parameter
+    /// - detail - The detail of the error. 
+    /// # Return
+    /// The newly created Boxed ClusterError.
+    pub fn detailled_boxed(detail: &str) -> Box<ClusterError> {
         Box::new(ClusterError {
-            detail: String::from(message),
+            detail: String::from(detail),
         })
     }
 
+    /// Create a Cluster error with no details
+    /// # Return
+    /// The newly created ClusterError
     pub fn new() -> ClusterError {
         ClusterError {
             detail: String::from(""),
         }
     }
 
+    /// Create a ClusterError with the specified details in it.
+    /// # Parameter
+    /// - detail - The detail of the error. 
+    /// # Return
+    /// The newly created ClusterError.
     pub fn detailled(message: &str) -> ClusterError {
         ClusterError {
             detail: String::from(message),
         }
     }
 
+    /// Returs the details of the ClusterError
+    /// # Returns
+    /// The detail of the ClusterError.
     pub fn message(&self) -> String {
         self.detail.clone()
     }
@@ -57,7 +76,13 @@ impl Default for ClusterError {
 }
 
 trait Node<K> {
+    /// Get the adjacency of the current Node.
+    /// # Return
+    /// A immutable reference to the adjacency list of the current Node.
     fn adj(&self) -> &Vec<K>;
+    /// Get the adjacency of the current Node.
+    /// # Return
+    /// A mutable reference to the adjacency list of the current Node.
     fn adj_mut(&mut self) -> &mut Vec<K>;
 }
 
@@ -69,15 +94,18 @@ where
     K: Clone,
 {
     /// Get a node from the graph
-    /// # Parameter
-    /// - key : the index of the node in the Graph.
-    /// Returns An option containing the Node if present in the Graph, returns None otherwise.
-    ///
+    /// **Parameter**
+    /// - key - the index of the node in the Graph.
+    /// 
+    /// # Return
+    /// An option containing an immutable reference to the Node if present in the Graph, returns None otherwise.
     fn get(&self, key: &K) -> Option<&N>;
 
     /// Get the adjancy list of the node designed by it key given in parameter.
     /// # Parameter
-    /// - key : the index of the node we want to get the adjacency list.
+    /// - key - the index of the node we want to get the adjacency list.
+    /// # Return
+    /// An immutable reference to the adjacency list of the desgnated node of the Cluster or None if there is no such Node.
     fn get_adj<'clu, 'res>(&'clu self, key: &K) -> Option<&'res Vec<K>>
     where
         'clu: 'res,
@@ -86,16 +114,44 @@ where
         self.get(key).map(|n| n.adj())
     }
 
+    /// Generate a key that is not already used in the graph and returns it.
+    /// # Return
+    /// The key newly generated.
     fn new_key(&self) -> K;
 
+    /// Check if the Cluster contains a node at a given key.
+    /// # Parameter
+    /// - key - The key on we want to check the Cluster contains it or no.
+    /// 
+    /// # Return
+    /// True if the key is in the Cluster, false otherwise.
     fn contains_key(&self, key: &K) -> bool;
 
+    /// Add a node in the Cluster.
+    /// # Return
+    /// The index at which the node has been stored in the graph.
     fn add(&mut self, node: N) -> K;
 
+    /// Removes the designated Node from the graph
+    /// # Parameter
+    /// - key - The key of the Node to remove.
+    /// # Return
+    /// An error if the node doesn't exist nothing otherwise.
     fn remove(&mut self, key: K) -> Result<()>;
 
+    /// Get a node from the graph
+    /// # Parameter
+    /// - key - the index of the node in the Graph.
+    /// 
+    /// # Return
+    /// An option containing an mutable reference to the Node if present in the Graph, returns None otherwise.
     fn get_mut(&mut self, key: &K) -> Option<&mut N>;
 
+    /// Get the adjancy list of the node designed by it key given in parameter.
+    /// # Parameter
+    /// - key - the index of the node we want to get the adjacency list.
+    /// # Return
+    /// A mutable reference to the adjacency list of the desgnated node of the Cluster or None if there is no such Node.
     fn get_adj_mut<'clu, 'res>(&'clu mut self, key: &K) -> Option<&'res mut Vec<K>>
     where
         'clu: 'res,
@@ -104,6 +160,14 @@ where
         self.get_mut(key).map(|n| n.adj_mut())
     }
 
+    /// Add an edge between src and dst in the Cluster.
+    /// # Parameters
+    /// - src - The key of the source node
+    /// - dst - The key of the destination node.
+    /// 
+    /// # Return
+    /// Nothing if everithing gone well, an error otherwise.
+    /// 
     fn add_edge(&mut self, src: K, dst: K) -> Result<()> {
         let adj = self.get_adj_mut(&src).ok_or(ClusterError::detailled_boxed(
             "To add edge, both node must exists in the Cluster.",
@@ -114,6 +178,14 @@ where
         Ok(())
     }
 
+    /// Remove the edge between src and dst in the Cluster.
+    /// # Parameters
+    /// - src - The key of the source node
+    /// - dst - The key of the destination node.
+    /// 
+    /// # Return
+    /// Nothing if everithing gone well, an error otherwise.
+    /// 
     fn remove_edge(&mut self, src: K, dst: K) -> Result<()> {
         let adj = self
             .get_adj_mut(&src)
@@ -124,12 +196,28 @@ where
         Ok(())
     }
 
+    /// Add an edge between src and dst in the Cluster in both directions.
+    /// # Parameters
+    /// - src - The key of the source node
+    /// - dst - The key of the destination node.
+    /// 
+    /// # Return
+    /// Nothing if everithing gone well, an error otherwise.
+    /// 
     fn add_doubly_edge(&mut self, src: K, dst: K) -> Result<()> {
         self.add_edge(src.clone(), dst.clone())?;
         self.add_edge(dst, src)?;
         Ok(())
     }
 
+    /// Reùove the edges between src and dst in the Cluster in both directions.
+    /// # Parameters
+    /// - src - The key of the source node
+    /// - dst - The key of the destination node.
+    /// 
+    /// # Return
+    /// Nothing if everithing gone well, an error otherwise.
+    /// 
     fn remove_doubly_edge(&mut self, src: K, dst: K) -> Result<()> {
         self.remove_edge(src.clone(), dst.clone())?;
         self.remove_edge(dst, src)?;
